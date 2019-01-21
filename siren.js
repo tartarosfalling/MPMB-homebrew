dfd28e9cff5eb13e611fe2c759bd6fef4ca721b2
tartarosfalling <46862521+tartarosfalling@users.noreply.github.com>
2019-01-20 17:55:21 -0800

    Update siren.js

--- siren.js
+++ siren.js
@@ -13,9 +13,9 @@ RaceList["siren"] = {
         swim : { spd : 30, enc : 20 },
         fly : { spd : 30, enc : 20 },
         climb : { spd : 15, enc : 5 },
-    },
+    		},
    
-    languageProfs :  ["Common (fluent)", "Aquan (fluent)", "Sylvan (comprehend and speak)", "Primordial (comprehend only)"],
+    languageProfs :  ["Common", "Aquan", "Sylvan", "Primordial"],
    
     vision : ["Darkvision", 120],
 
@@ -23,10 +23,9 @@ RaceList["siren"] = {
 
     savetxt : { 
           // immune : ["lightning", "thunder", "cold", "pressure"], 
-					// adv_vs : ["charm", "mind control"],
-          text: ["Immune to environmental effects of cold, lightning, pressure, and thunder;\n Adv. on saves vs. charm and mind control"],
-          
-		},
+	  // adv_vs : ["charm", "mind control"],
+          text: ["Immune to environmental effects of cold, lightning, pressure, and thunder;\nAdv. on saves vs. charm and mind control"],
+          },
    
     skills : ["Persuasion", "Deception"],
  
@@ -34,7 +33,7 @@ RaceList["siren"] = {
    
     scores : [0, 0, 0, 0, 0, 2],
 
-    trait : "Siren (+2 Charisma)\nAmphibious:\n    I can breathe both air and water with equal ease.\nEmissary of the Sea:\n    I can communicate simple ideas to beasts that can breathe water. They can understand the meaning of my words, though I have no special ability to understand them in return.\n Siren Song:\n    I can innately cast the following spells, requiring no material components: charm, invisibility, stupefying touch\nElemental Touch:\n    I know Fog Cloud. Once I reach 3rd level, I can Gust of Wind with it. Once I reach 5th level, I can cast Wall of Water with it.",
+    trait : "Siren (+2 Charisma)\nAmphibious: I can breathe both air and water with equal ease.\nEmissary of the Sea:\n    I can communicate simple ideas to beasts that can breathe water. They can understand the meaning of my words, though I have no special ability to understand them in return.\nSiren Song:\n    I can innately cast the following spells, requiring no material components: charm, invisibility, stupefying touch\nElemental Touch:\n    I can innately cast Fog Cloud. Once I reach 3rd level, I can cast Gust of Wind with it. Once I reach 5th level, I can cast Wall of Water with it.",
 	
     abilitySave : 6,
  
@@ -51,7 +50,7 @@ RaceList["siren"] = {
 		  spells : ["charm person", "invisibility", "stupefying touch"], //Optional, but required if not including the "class" entry; If a "spells" array is present, all other objects will be ignored and only this list of spells will populate the list of available spells. each entry has to match the name of the spell in the SpellsList
 		
 		  // times : 3, 
-      //optional; this is the number of times the bonus spells should be added. //This can also be an array of 20 values. That way the number of times are level-dependent
+      		  //optional; this is the number of times the bonus spells should be added. //This can also be an array of 20 values. That way the number of times are level-dependent
 		
 		  prepared : true, 
 		
@@ -82,10 +81,9 @@ RaceList["siren"] = {
 			name : "gust of wind", 
 			minlevel : 3, 
 			usages : 1, 
-      prepared : true,
+      			prepared : true,
 			recovery : "long rest", 
 			tooltip : "Elemental Touch (level 3)",
-			action : ["action", ""], 
 			spellcastingBonus : { 
 				name : "Elemental Touch (level 3)",
 				spells : ["gust of wind"],
@@ -98,10 +96,9 @@ RaceList["siren"] = {
 			name : "wall of water", 
 			minlevel : 5, 
 			usages : 1, 
-      prepared : true,
+      			prepared : true,
 			recovery : "long rest", 
 			tooltip : "Elemental Touch (level 5)",
-			action : ["action", ""], 
 			spellcastingBonus : { 
 				name : "Elemental Touch (level 5)",
 				spells : ["wall of water"],
